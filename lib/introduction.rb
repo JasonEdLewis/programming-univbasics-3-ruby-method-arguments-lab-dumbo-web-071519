532170e8f5440572b8dd7076f7c240ec6d055d0d
Jason Lewis <jasonedlewis@gmail.com>
2019-07-01 01:46:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,6 +5,6 @@ def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 def introduction_with_language_optional(name,language)
-  language == " "? (puts "Hi, my name is #{name} and I am learning to program in #{language}.") : (puts "Hi, my name is #{name} and I am learning to program in Ruby.") end
+  language == "" ? (puts "Hi, my name is #{name} and I am learning to program in #{language}.") : (puts "Hi, my name is #{name} and I am learning to program in Ruby.") end
     
 end
